146ef1ef94ad808627b0a9f94d34d07aab1016ab
smartpeople <semeyon@speople.pro>
2017-05-05 14:43:11 +0600

    Optimizations

---
+++ optimizations.py
@@ -0,0 +1,53 @@
+from keras.datasets import mnist # subroutines for fetching the MNIST dataset
+from keras.models import Model # basic class for specifying and training a neural network
+from keras.layers import Input, Dense, Flatten, Convolution2D, MaxPooling2D, Dropout
+from keras.utils import np_utils # utilities for one-hot encoding of ground truth values
+
+batch_size = 128 # in each iteration, we consider 128 training examples at once
+num_epochs = 12 # we iterate twelve times over the entire training set
+kernel_size = 3 # we will use 3x3 kernels throughout
+pool_size = 2 # we will use 2x2 pooling throughout
+conv_depth = 32 # use 32 kernels in both convolutional layers
+drop_prob_1 = 0.25 # dropout after pooling with probability 0.25
+drop_prob_2 = 0.5 # dropout in the FC layer with probability 0.5
+hidden_size = 128 # there will be 128 neurons in both hidden layers
+
+num_train = 60000 # there are 60000 training examples in MNIST
+num_test = 10000 # there are 10000 test examples in MNIST
+
+height, width, depth = 28, 28, 1 # MNIST images are 28x28 and greyscale
+num_classes = 10 # there are 10 classes (1 per digit)
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data() # fetch MNIST data
+
+X_train = X_train.reshape(X_train.shape[0], depth, height, width)
+X_test = X_test.reshape(X_test.shape[0], depth, height, width)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255 # Normalise data to [0, 1] range
+X_test /= 255 # Normalise data to [0, 1] range
+
+Y_train = np_utils.to_categorical(y_train, num_classes) # One-hot encode the labels
+Y_test = np_utils.to_categorical(y_test, num_classes) # One-hot encode the labels
+
+inp = Input(shape=(depth, height, width)) # N.B. Keras expects channel dimension first
+# Conv [32] -> Conv [32] -> Pool (with dropout on the pooling layer)
+conv_1 = Convolution2D(conv_depth, kernel_size, kernel_size, border_mode='same', activation='relu')(inp)
+conv_2 = Convolution2D(conv_depth, kernel_size, kernel_size, border_mode='same', activation='relu')(conv_1)
+pool_1 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_2)
+drop_1 = Dropout(drop_prob_1)(pool_1)
+flat = Flatten()(drop_1)
+hidden = Dense(hidden_size, activation='relu')(flat) # Hidden ReLU layer
+drop = Dropout(drop_prob_2)(hidden)
+out = Dense(num_classes, activation='softmax')(drop) # Output softmax layer
+
+model = Model(input=inp, output=out) # To define a model, just specify its input and output layers
+
+model.compile(loss='categorical_crossentropy', # using the cross-entropy loss function
+              optimizer='adam', # using the Adam optimiser
+              metrics=['accuracy']) # reporting the accuracy
+
+model.fit(X_train, Y_train, # Train the model using the training set...
+          batch_size=batch_size, nb_epoch=num_epochs,
+          verbose=1, validation_split=0.1) # ...holding out 10% of the data for validation
+model.evaluate(X_test, Y_test, verbose=1) # Evaluate the trained model on the test set!
